f1ee0a9c61e43401f7ec4dadaf0defa8d77c6c68
Nicholas Tom <99391779+NickTom7@users.noreply.github.com>
2022-11-12 15:27:33 -1000

    update landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Carousel, Image, Container } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" fluid className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to Quizzacious!</h1>
-        <p>Create, Take and Rate quizzes to help you study!</p>
-      </Col>
-
-    </Row>
+  <Container className="text-center align-items-center">
+    <h4>React-Bootstrap Carousel Component</h4>
+    <Carousel>
+      <Carousel.Item interval={1500}>
+        <Image
+          className="d-block w-100"
+          src="/images/3.jpg"
+          alt="Image One"
+        />
+        <Carousel.Caption>
+          <h3>Label for first slide</h3>
+          <p>Sample Text for Image One</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={1500}>
+        <Image
+          className="d-block w-100"
+          src="/images/4.jpg"
+          alt="Image Two"
+        />
+        <Carousel.Caption>
+          <h3>Label for second slide</h3>
+          <p>Sample Text for Image Two</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   </Container>
 );
 
