4ccacb84ea5f20eeb8e8022b2bb0b9feee15a4b5
JeromeWasserman <46459313+JeromeWasserman@users.noreply.github.com>
2022-11-15 20:55:24 -1000

    Landing page mockup done
    done just need to fill in some empty spaces in the bottom, maybe a carousel for fake ads or something

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
 import { Roles } from 'meteor/alanning:roles';
-import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown, Image, Stack } from 'react-bootstrap';
 
 const NavBar = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
@@ -14,11 +14,11 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand>
-          <Image src="https://media.discordapp.net/attachments/1039357989527760963/1041216966242873394/image.png?width=491&height=585" height="100px" />
-        </Navbar.Brand>
-        <Navbar.Brand>
-          <h1>UIZZACIOUS</h1>
+        <Navbar.Brand as={NavLink} to="/home" key="home">
+          <Stack direction="horizontal">
+            <Image src="https://media.discordapp.net/attachments/1039357989527760963/1041216966242873394/image.png?width=491&height=585" height="100px" />
+            <h1>UIZZACIOUS</h1>
+          </Stack>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
-import { Carousel, Image, Container } from 'react-bootstrap';
+import { Carousel, Container, Card } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container className="text-center align-items-center">
-    <h4>React-Bootstrap Carousel Component</h4>
-    <Carousel>
-      <Carousel.Item interval={1500}>
-        <Image
-          className="d-block w-100"
-          src="/images/3.jpg"
-          alt="Image One"
-        />
-        <Carousel.Caption>
-          <h3>Label for first slide</h3>
-          <p>Sample Text for Image One</p>
-        </Carousel.Caption>
+  <Container className="text-center align-items-center py-4">
+    <h4 className="py-2 text-white font2">Words to Motivate and Start your Day with a Smile!</h4>
+    <Carousel className="text-white">
+      <Carousel.Item>
+        <Card style={{ height: '18rem' }}>
+          <Card.Img src="https://media.tenor.com/f6Z_JUiELaMAAAAC/winter-wonderland-snow.gif" />
+          <Card.ImgOverlay>
+            <h1 className="py-2"><strong>Over the Wintry by Natsume Soseki</strong></h1>
+            <h1>Over the wintry</h1>
+            <h1>Forest, winds howl in rage</h1>
+            <h1>With no leaves to blow.</h1>
+          </Card.ImgOverlay>
+        </Card>
       </Carousel.Item>
-      <Carousel.Item interval={1500}>
-        <Image
-          className="d-block w-100"
-          src="/images/4.jpg"
-          alt="Image Two"
-        />
-        <Carousel.Caption>
-          <h3>Label for second slide</h3>
-          <p>Sample Text for Image Two</p>
-        </Carousel.Caption>
+      <Carousel.Item>
+        <Card style={{ height: '18rem' }}>
+          <Card.Img src="https://media.tenor.com/M4uxQgC_uOEAAAAd/flower-bloom.gif" />
+          <Card.ImgOverlay>
+            <h1 className="py-2"><strong>A Poppy Blooms by Katsushika Hokusai</strong></h1>
+            <h1>I write, erase, rewrite</h1>
+            <h1>Erase again, and then</h1>
+            <h1>A poppy blooms.</h1>
+          </Card.ImgOverlay>
+        </Card>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Card style={{ height: '18rem' }}>
+          <Card.Img src="https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/media/images/Oceanwaves.gif" alt="Card image" />
+          <Card.ImgOverlay>
+            <h1 className="py-2"><strong>Spring Ocean by Yosa Buson</strong></h1>
+            <h1>Spring ocean</h1>
+            <h1>Swaying gently</h1>
+            <h1>All day long.</h1>
+          </Card.ImgOverlay>
+        </Card>
       </Carousel.Item>
     </Carousel>
   </Container>
