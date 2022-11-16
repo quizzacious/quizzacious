d5ac1001dc594d9900c220382565fe7c4a174988
JeromeWasserman <46459313+JeromeWasserman@users.noreply.github.com>
2022-11-15 19:31:18 -1000

    Navbar Mockup Done
    Still need to finish some spacing done

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,8 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
 import { Roles } from 'meteor/alanning:roles';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { BoxArrowRight } from 'react-bootstrap-icons';
+import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 
 const NavBar = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
@@ -14,9 +13,12 @@ const NavBar = () => {
 
   return (
     <Navbar bg="light" expand="lg">
-      <Container className="font">
+      <Container>
         <Navbar.Brand>
-          <h2>QUIZZACIOUS</h2>
+          <Image src="https://media.discordapp.net/attachments/1039357989527760963/1041216966242873394/image.png?width=491&height=585" height="100px" />
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <h1>UIZZACIOUS</h1>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -25,23 +27,19 @@ const NavBar = () => {
               <Nav.Link id="menu-quiz-nav" as={NavLink} to="/menu" key="menu">Make-A-Quiz</Nav.Link>,
               <Nav.Link id="take-quiz--nav" as={NavLink} to="/take" key="take">Take Quiz</Nav.Link>,
               <Nav.Link id="profile-nav" as={NavLink} to="/profile" key="profile">Profile</Nav.Link>,
+            ]) : ''}
+            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+              <Nav.Link id="admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
+            ) : ''}
+          </Nav>
+          <Nav className="me-auto justify-content-end">
+            {currentUser ? ([
               <NavDropdown id="navbar-current-user" title={currentUser}>
                 <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
-                  <BoxArrowRight />
-                  {' '}
-                  Sign
-                  out
+                  Sign Out
                 </NavDropdown.Item>
               </NavDropdown>,
             ]) : ''}
-            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Nav.Link id="admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>, 
-              <Nav className="justify-content-end">
-                  <NavDropdown id="navbar-current-user" title={currentUser}>
-                    <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">Sign out</NavDropdown.Item>
-                  </NavDropdown>
-              </Nav>
-            ) : ''}
           </Nav>
         </Navbar.Collapse>
       </Container>
