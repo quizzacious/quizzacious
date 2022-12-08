ff88adbc675b6e36e2c71dd014b6911566137a6a
brycemao123 <brycemao@hawaii.edu>
2022-12-08 09:08:13 -1000

    Create Score.jsx

---
+++ app/imports/ui/pages/Score.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import swal from 'sweetalert';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, SelectField, SubmitField } from 'uniforms-bootstrap5';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import SimpleSchema from 'simpl-schema';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { useParams } from 'react-router';
+import LoadingSpinner from '../components/LoadingSpinner';
+import { TakenQuizzes } from '../../api/takenquiz/TakenQuizzes';
+
+const bridge = new SimpleSchema2Bridge(new SimpleSchema({ answerFinal: String }));
+
+/* Renders the QuizPage page for preparing the actual quiz. */
+const Score = () => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const { _id, take_id, num } = useParams();
+  // console.log('QuizPage', _id);
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { doc, ready } = useTracker(() => {
+    // Get access to Quizzes documents.
+    const subscription = Meteor.subscribe(TakenQuizzes.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the document
+    const quiz = TakenQuizzes.collection.findOne(_id);
+    // DELETE MAYBE??? const takenQuiz = TakenQuizzes.collection.findOne(take_id);
+    return {
+      doc: quiz,
+      ready: rdy,
+    };
+  }, [_id]);
+  // console.log('QuizPage', doc, ready);
+  // On successful submit, insert the data.
+  const submit = (data) => {
+    const { answerFinal } = data;
+    TakenQuizzes.collection.update(take_id, { $set: { 'inputtedAnswers.$[num]': answerFinal } }, { arrayFilters: [{ num: num }] }, (error) => (error ?
+      swal('Error', error.message, 'error') :
+      swal('Success', 'Item updated successfully', 'success')));
+  };
+
+  return ready ? (
+    <Container id="takingquiz" className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={5}>
+          <Col className="text-center"><h2>Quiz</h2></Col>
+          <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
+            <Card>
+              <Card.Body>
+                <SelectField name="answerFinal" />
+                <SubmitField value="Submit" />
+                <ErrorsField />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />;
+};
+
+export default Score;
