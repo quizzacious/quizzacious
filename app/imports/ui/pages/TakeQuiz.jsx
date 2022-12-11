629a4b1872a25cc679c3d0a8f062030264b7b4fa
brycemao123 <brycemao@hawaii.edu>
2022-12-10 20:16:40 -1000

    Finished Filter Functionality

--- app/imports/ui/pages/TakeQuiz.jsx
+++ app/imports/ui/pages/TakeQuiz.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col, Container, Form, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
+import { Link } from 'react-router-dom';
 import QuizItem from '../components/QuizItem';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Quizzes } from '../../api/quiz/Quizzes';
-// import Filter from '../components/Filter';
 
 /* Renders a table containing all of the Quizzes documents. Use <QuizItem> to render each row. */
 const TakeQuiz = () => {
-  const OnChangeValue = () => {
-    // eslint-disable-next-line no-restricted-globals
-    console.log(event.target.value);
-    // eslint-disable-next-line no-restricted-globals
-    return event.target.value;
-  };
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, stuffs: quizzes } = useTracker(() => {
+  const { ready, quizzes } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Quizzes documents.
@@ -26,7 +20,7 @@ const TakeQuiz = () => {
     // Get the Quizzes documents
     const quizItems = Quizzes.collection.find({}).fetch();
     return {
-      stuffs: quizItems,
+      quizzes: quizItems,
       ready: rdy,
     };
   }, []);
@@ -36,20 +30,10 @@ const TakeQuiz = () => {
         <Col>
           <Col className="text-center">
             <h2>Take Quiz</h2>
+            <Link className="p-4" to="/filter">Filter</Link>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
-            <Form.Control
-              as="select"
-              custom
-              /* eslint-disable-next-line react/jsx-no-bind */
-              onChange={OnChangeValue.bind(this)}
-            >
-              <option>Filter by Subject</option>
-              <option value="Math">Math</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </Form.Control>
-            {quizzes.filter(quiz => quiz.subject === 'Math').map((quiz, index) => (<Col key={index}><QuizItem quiz={quiz} /></Col>))}
+            {quizzes.map((quiz, index) => (<Col key={index}><QuizItem quiz={quiz} /></Col>))}
           </Row>
         </Col>
       </Row>
