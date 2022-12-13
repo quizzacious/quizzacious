671a0156cb9f5c55e30a4377346d6343d2de427d
Nicholas Tom <99391779+NickTom7@users.noreply.github.com>
2022-12-13 10:00:36 -1000

    eslint

--- app/imports/ui/components/ListQuestionItem.jsx
+++ app/imports/ui/components/ListQuestionItem.jsx
@@ -3,21 +3,19 @@ import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const ListQuestionItem = ({ question, collection }) => {
-  return (
-    <Card className="h-100">
-      <Card.Body>
-        <Card.Text>
-          <p>Question: {question.question}</p>
-          <p>Answer 1: {question.answer1}</p>
-          <p>Answer 2: {question.answer2}</p>
-          <p>Answer 3: {question.answer3}</p>
-          <p>Answer 4: {question.answer4}</p>
-          <p>Correct Answer: {question.answerFinal}</p>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  );
+const ListQuestionItem = ({ question }) => {
+  <Card className="h-100">
+    <Card.Body>
+      <Card.Text>
+        <p>Question: {question.question}</p>
+        <p>Answer 1: {question.answer1}</p>
+        <p>Answer 2: {question.answer2}</p>
+        <p>Answer 3: {question.answer3}</p>
+        <p>Answer 4: {question.answer4}</p>
+        <p>Correct Answer: {question.answerFinal}</p>
+      </Card.Text>
+    </Card.Body>
+  </Card>;
 };
 
 // Require a document to be passed to this component.
@@ -33,7 +31,6 @@ ListQuestionItem.propTypes = {
     questionNum: PropTypes.number,
   }).isRequired,
   // eslint-disable-next-line react/forbid-prop-types
-  collection: PropTypes.object.isRequired,
 };
 
 export default ListQuestionItem;
